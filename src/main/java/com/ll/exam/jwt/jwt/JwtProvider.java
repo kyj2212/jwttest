04d70c89cca79a6c6c3af7681f483de3aa5ce40f
kyj2212 <kyj2212@gmail.com>
2022-10-04 22:53:19 +0900

    Style: add debug log

--- src/main/java/com/ll/exam/jwt/jwt/JwtProvider.java
+++ src/main/java/com/ll/exam/jwt/jwt/JwtProvider.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -26,6 +27,7 @@ public class JwtProvider {
 
     @PostConstruct
     private void init(){
+        System.out.println("[PostConstruct] init Jwt secret key");
         String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
         jwtSecretKey= Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
     }
