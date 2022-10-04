f447389b9289137f853cb382b6f627197034889b
kyj2212 <kyj2212@gmail.com>
2022-10-04 14:18:41 +0900

    Feat: secret key in application.yml custom value

--- src/test/java/com/ll/exam/jwt/JwtApplicationTests.java
+++ src/test/java/com/ll/exam/jwt/JwtApplicationTests.java
@@ -1,13 +1,25 @@
 package com.ll.exam.jwt;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtApplicationTests {
 
+	@Value("${custom.jwt.secretKey}")
+	private String secretKey;
+
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	@DisplayName("SecretKey가 존재해야 한다.")
+	void t1() {
+		assertThat(secretKey).isNotNull();
 	}
 
 }
