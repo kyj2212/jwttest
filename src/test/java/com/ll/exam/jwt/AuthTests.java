dc5b5efb60dbb0e319aeb188671d26a7a12dcafc
kyj2212 <kyj2212@gmail.com>
2022-10-05 15:35:21 +0900

    Feat: add response header form mockhttpservlet

--- src/test/java/com/ll/exam/jwt/AuthTests.java
+++ src/test/java/com/ll/exam/jwt/AuthTests.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +20,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,6 +38,8 @@ class AuthTests {
         ResultActions resultActions = mvc
                 .perform(
                         post("/member/login").with(csrf())
+
+                                .header("Authentication","Bearer (accessToken")
                                 .content("""
                                         {
                                             "username": "user1",
@@ -40,11 +47,21 @@ class AuthTests {
                                         }
                                         """.stripIndent())
                                 .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+
                 )
                 .andDo(print());
+
 
         // Then
         resultActions
                 .andExpect(status().is2xxSuccessful());
+
+        MvcResult mvcResult = resultActions.andReturn();
+
+        MockHttpServletResponse mockHttpServletResponse = mvcResult.getResponse();
+        mockHttpServletResponse.addHeader("Authentication",mvcResult.getRequest().getHeader("Authentication"));
+
+        String authentication = mockHttpServletResponse.getHeader("Authentication");
+        assertThat(authentication).isNotEmpty();
     }
 }
